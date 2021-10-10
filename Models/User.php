7f4756409daa2011deb09536a152c969c79a29a8
Ali Kazmi <smurtazakazmi14@gmail.com>
2021-10-10 12:04:38 +0500

    Revert "Updates user model to add fillable column"

--- Models/User.php
+++ Models/User.php
@@ -7,7 +7,6 @@ use App\Containers\Vendor\Beaner\Traits\HasLocations;
 use App\Containers\Vendor\Beaner\Traits\HasMobileLocation;
 use App\Containers\Vendor\Beaner\Traits\HasUuid;
 use App\Containers\Vendor\Beaner\Traits\MultiTenantable;
-use Illuminate\Support\Arr;
 
 class User extends ApiatoUser
 {
@@ -15,26 +14,4 @@ class User extends ApiatoUser
     use MultiTenantable;
     use HasLocations;
     use HasMobileLocation;
-
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'device',
-        'platform',
-        'gender',
-        'birth',
-        'social_provider',
-        'social_token',
-        'social_refresh_token',
-        'social_expires_in',
-        'social_token_secret',
-        'social_id',
-        'social_avatar',
-        'social_avatar_original',
-        'social_nickname',
-        'email_verified_at',
-        'is_admin',
-        'tenant_id'
-    ];
 }

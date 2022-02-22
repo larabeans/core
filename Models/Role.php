fe7628a905b3de8b7fdc4cb4810a499b4075fb0e
Sajjad Naqvi <sajjad@orenda.systems>
2022-02-22 15:39:12 +0500

    Updates files to comment HasTenancy trait which causing error on installation

--- Models/Role.php
+++ Models/Role.php
@@ -3,13 +3,13 @@
 namespace App\Containers\Vendor\Beaner\Models;
 
 use App\Containers\AppSection\Authorization\Models\Role as ApiatoRole;
-use App\Containers\Vendor\Uuider\Traits\HasUuid;
+use App\Containers\Vendor\Beaner\Traits\HasUuid;
 use App\Containers\Vendor\Tenanter\Models\Concerns\HasTenancy;
 
 class Role extends ApiatoRole
 {
     use HasUuid;
-    use HasTenancy;
+    //use HasTenancy; // UnComment if the tenanter package is installed before migration
 
     protected $fillable = [
         'name',
--- Parents/Models/Model.php
+++ Parents/Models/Model.php
@@ -3,11 +3,11 @@
 namespace App\Containers\Vendor\Beaner\Parents\Models;
 
 use App\Ship\Parents\Models\Model as ApiatoParentModel;
-use App\Containers\Vendor\Uuider\Traits\HasUuid;
+use App\Containers\Vendor\Beaner\Traits\HasUuid;
 use App\Containers\Vendor\Tenanter\Models\Concerns\HasTenancy;
 
 abstract class Model extends ApiatoParentModel
 {
     use HasUuid;
-    use HasTenancy;
+    //use HasTenancy;  // UnComment if the tenanter package is installed before migration
 }
